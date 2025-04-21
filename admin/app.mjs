5ea636735b1d5ff275796851a0602f4f70c3efc8
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-04-22 00:37:35 +0500

    set cors

--- admin/app.mjs
+++ admin/app.mjs
@@ -12,16 +12,22 @@ import path from 'path';
 const app = express();
 dotenv.config();
 
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
 const allowedOrigins = [
   "https://jpcorporation-production.up.railway.app",
   "http://localhost:5173",
+  "http://localhost:5174",
   // "http://localhost:3000",
   // "http://localhost:5000",
   // "http://localhost:8000",
-  "http://localhost:8800",
+  // "http://localhost:8800",
   "https://jp-corporation-o2co.vercel.app/"
 ];
-
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -35,25 +41,21 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsOptions));
-const __dirname = path.resolve();
-
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, '../dist')));
-
-app.use("/api/cards", carRoutes);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/api/cards", carRoutes);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/dashboard" , dashboardRoutes)
 app.use("/api/admin" , adminRoutes)
 app.use("/api/capLinks" , capLinksRoutes)
 app.use("/api/model" , modelRoutes)
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
 
+const __dirname = path.resolve();
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, '../dist')));
 // Routes Example
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
